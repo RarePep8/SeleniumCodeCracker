d48bc24fa011f8d8e004c100fcfa350b23eb53f3
RarePep8 <alex.wong.utsc@gmail.com>
2018-02-24 02:00:46 -0500

    Finished

--- cracker.py
+++ cracker.py
@@ -1,32 +1,27 @@
 from selenium import webdriver
+import time
 browser = webdriver.Chrome()
 browser.get('https://live.hackvalley2.com/500hash')
 hash_box = browser.find_element_by_id("hash-box")
 submit_button = browser.find_element_by_id("get-button")
 result = browser.find_element_by_id('result')
 
-hash_box.clear()
-hash_box.send_keys("hi")
-submit_button.click()
 
 
-hash_box.clear()
-hash_box.send_keys("fuck this shit")
-submit_button.click()
 
 
-print(result.get_attribute("text"))
-print(submit_button.text)
 
 ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
-def combinations_crack(curr_seq):
+def combination_crack(curr_seq):
     if(len(curr_seq) < 10):
         for letter in ALPHABET:
-            result = combinations_crack(curr_seq.append(letter)) 
+            combination_crack(curr_seq + letter) 
     else:
         hash_box.clear()
-        hash_box.send_keys(result)
+        hash_box.send_keys(curr_seq)
         submit_button.click()
-        
-    
-    
+        time.sleep(0.25)
+        temp = result.get_attribute("outerHTML")
+        if("Hash" not in temp):
+            print(temp[-12])
+combination_crack("")
